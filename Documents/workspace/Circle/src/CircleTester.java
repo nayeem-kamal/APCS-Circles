44644e31774e2fb141d033bdb072692ccee00952
nayeem-kamal <kamal.nayeem12@gmail.com>
2017-02-11 10:08:22 -0500

    first commit

---
+++ Documents/workspace/Circle/src/CircleTester.java
@@ -0,0 +1,12 @@
+
+public class CircleTester {
+	public static void main(String[] args){
+	Circle a = new Circle(0,0,10);
+	System.out.println(a.getArea());
+	System.out.println(a.getCircumference());
+	
+
+
+	
+	}	
+}
